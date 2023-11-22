fc516448c85557f0c767cfc74583d026218d0dc4
Komalpreet Singh <komalpreetsingh2305@gmail.com>
2023-11-22 02:43:37 -0500

    Update input.js

--- input.js
+++ input.js
@@ -26,3 +26,73 @@ export function getInputDirection() {
     lastInputDirection = inputDirection;
     return inputDirection;
 }
+
+
+// New code for handling touch input
+let touchStartX, touchStartY;
+document.addEventListener('touchstart', handleTouchStart);
+document.addEventListener('touchmove', handleTouchMove);
+
+function handleTouchStart(event) {
+    // Store the initial touch coordinates
+    touchStartX = event.touches[0].clientX;
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    if (!touchStartX || !touchStartY) {
+        return;
+    }
+
+    // Calculate the change in coordinates
+    const touchEndX = event.touches[0].clientX;
+    const touchEndY = event.touches[0].clientY;
+
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
+
+    // Determine the direction based on the change in coordinates
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        // Horizontal swipe
+        if (deltaX > 0) {
+            // Swipe right
+            handleSwipeRight();
+        } else {
+            // Swipe left
+            handleSwipeLeft();
+        }
+    } else {
+        // Vertical swipe
+        if (deltaY > 0) {
+            // Swipe down
+            handleSwipeDown();
+        } else {
+            // Swipe up
+            handleSwipeUp();
+        }
+    }
+
+    // Reset the initial touch coordinates
+    touchStartX = null;
+    touchStartY = null;
+}
+
+function handleSwipeRight() {
+    // Handle right swipe
+    // ...
+}
+
+function handleSwipeLeft() {
+    // Handle left swipe
+    // ...
+}
+
+function handleSwipeDown() {
+    // Handle down swipe
+    // ...
+}
+
+function handleSwipeUp() {
+    // Handle up swipe
+    // ...
+}
